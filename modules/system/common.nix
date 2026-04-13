5087225b31dbefe57050b1f810968f58c94ee958
kierandrewett <kieran@drewett.dev>
2026-04-13 22:46:38 +0100

    chore: drop custom bingux-cli, ensure comma is in base system

--- modules/system/common.nix
+++ modules/system/common.nix
@@ -42,7 +42,7 @@
         programs.zsh.enable = lib.mkDefault true;
 
         # Bingux CLI helper + essentials
-        environment.systemPackages = with pkgs; [ os-helper fastfetch ];
+        environment.systemPackages = with pkgs; [ os-helper fastfetch comma ];
 
         # Mark /os as safe for git
         environment.etc."gitconfig".text = lib.mkDefault ''
